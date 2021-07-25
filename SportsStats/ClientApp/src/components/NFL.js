65df31fd3065933cac229d59ec0624f51a431267
DallinBreen <86697642+DallinBreen@users.noreply.github.com>
2021-07-25 17:04:23 -0600

    Update Front-End
    Updated NavBar to display links to Home page and NFL page
    
    NFL page will be used to locate teams in the league and show their stats

---
+++ SportsStats/ClientApp/src/components/NFL.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+export class NFL extends Component {
+    static displayName = NFL.name;
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+            </div>
+        );
+    }
+}
