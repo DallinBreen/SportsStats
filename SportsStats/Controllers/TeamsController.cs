ea2cd2d6113ee3422b99456b350e905a2c9ebf90
DallinBreen <86697642+DallinBreen@users.noreply.github.com>
2021-07-27 19:10:50 -0600

    Added HttpClient to team controller and got all teams

--- SportsStats/Controllers/TeamsController.cs
+++ SportsStats/Controllers/TeamsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,7 @@ namespace SportsStats.Controllers
    
     public class TeamsController : Controller
     {
+        static readonly HttpClient client = new HttpClient();
 
         private readonly ILogger<TeamsController> _logger;
 
@@ -19,9 +21,23 @@ namespace SportsStats.Controllers
             _logger = logger;
         }
 
-        public string Index()
+        public async Task<string> Index()
         {
-            return "All Teams";
+            try
+            {
+                String response = await client.GetStringAsync("https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4391");
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                Console.WriteLine(response);
+                return response;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return e.Message;
+            }
         }
 
         
